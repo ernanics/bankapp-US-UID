d728b3d5f9bf313acbd48e17d010b9562e071253
AdanDuM <adandum@hotmail.com>
2019-02-02 18:21:34 -0200

    8 - Calcular valor das parcelas de um financiamento (EMI)

--- src/test/java/br/ufsc/bank/usuid/atm/loan/LoanEntradaValoresFixture.java
+++ src/test/java/br/ufsc/bank/usuid/atm/loan/LoanEntradaValoresFixture.java
@@ -6,6 +6,81 @@ import net.douglashiura.us.Fixture;
 
 @Fixture("LoanEntradaValoresFixture")
 public class LoanEntradaValoresFixture {
+
+	public Loan loan1 = new Loan();
+	public Loan loan2 = new Loan();
+	public Loan loan3 = new Loan();
+	public Loan loan4 = new Loan();
 	
+	public void toLoanSimulacaoFixture() {
+		LoanFeature.loan.add(loan1);
+		LoanFeature.loan.add(loan2);
+		LoanFeature.loan.add(loan3);
+		LoanFeature.loan.add(loan4);
+	}
 	
+	public String getValorFinanciado() {
+		return "valor financiado";
+	}
+	
+	public String getPcJuros() {
+		return "pc juros";
+	}
+	
+	public String getNuParcelas() {
+		return "nu. de parcelas";
+	}
+	
+	public void setValorFinanciado1(String valorFinanciado1) {
+		loan1.setAmount(Double.parseDouble(valorFinanciado1));
+	}
+	
+	public void setPcJuros1(String pcJuros1) {
+		loan1.setPcOfInterest(Double.parseDouble(pcJuros1));
+	}
+	
+	public void setNuParcelas1(String nuParcelas1) {
+		loan1.setNumberOfMontlhyInstalments(Long.parseLong(nuParcelas1));
+	}
+	
+	public void setValorFinanciado2(String valorFinanciado2) {
+		loan2.setAmount(Double.parseDouble(valorFinanciado2));
+	}
+	
+	public void setPcJuros2(String pcJuros2) {
+		loan2.setPcOfInterest(Double.parseDouble(pcJuros2));
+	}
+	
+	public void setNuParcelas2(String nuParcelas2) {
+		loan2.setNumberOfMontlhyInstalments(Long.parseLong(nuParcelas2));
+	}
+	
+	public void setValorFinanciado3(String valorFinanciado3) {
+		loan3.setAmount(Double.parseDouble(valorFinanciado3));
+	}
+	
+	public void setPcJuros3(String pcJuros3) {
+		loan3.setPcOfInterest(Double.parseDouble(pcJuros3));
+	}
+	
+	public void setNuParcelas3(String nuParcelas3) {
+		loan3.setNumberOfMontlhyInstalments(Long.parseLong(nuParcelas3));
+	}
+	
+	public void setValorFinanciado4(String valorFinanciado4) {
+		loan4.setAmount(Double.parseDouble(valorFinanciado4));
+	}
+	
+	public void setPcJuros4(String pcJuros4) {
+		loan4.setPcOfInterest(Double.parseDouble(pcJuros4));
+	}
+	
+	public void setNuParcelas4(String nuParcelas4) {
+		loan4.setNumberOfMontlhyInstalments(Long.parseLong(nuParcelas4));
+	}
+	
+	public void setCalcular(String calcular) {
+
+	}
+
 }
--- src/test/java/br/ufsc/bank/usuid/atm/loan/LoanSimulacaoFixture.java
+++ src/test/java/br/ufsc/bank/usuid/atm/loan/LoanSimulacaoFixture.java
@@ -1,6 +1,6 @@
 package br.ufsc.bank.usuid.atm.loan;
 
-import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 import br.ufsc.bank.usuid.LoanFeature;
 import net.douglashiura.us.Fixture;
@@ -8,4 +8,110 @@ import net.douglashiura.us.Fixture;
 @Fixture("LoanSimulacaoFixture")
 public class LoanSimulacaoFixture {
 	
+	public DecimalFormat format1 = new DecimalFormat("###.00");
+	public DecimalFormat format2 = new DecimalFormat("##0.0000");
+	
+	public String getResultadoSimulacoes() {
+		return "Resultado das Simulacoes";
+	}
+	
+	public String getValorFinanciado() {
+		return "valor financiado";
+	}
+	
+	public String getPcJuros() {
+		return "pc juros";
+	}
+	
+	public String getNuParcelas() {
+		return "nu. de parcelas";
+	}
+	
+	public String getVlParcelas() {
+		return "valor das parcelas";
+	}
+	
+	public String getValorFinanciadoComJuros() {
+		return "valor total financiado com juros";
+	}
+	
+	public String getValorFinanciado1() {
+		return format1.format(LoanFeature.loan.get(0).getAmount()).replace(',', '.');
+	}
+	
+	public String getPcJuros1() {
+		return format2.format(LoanFeature.loan.get(0).getPcOfInterest()).replace(',', '.');
+	}
+	
+	public String getNuParcelas1() {
+		return LoanFeature.loan.get(0).getNumberOfMontlhyInstalments().toString();
+	}
+	
+	public String getVlParcelas1() {
+		return format1.format(LoanFeature.loan.get(0).getValueOfMontlhyInstalments()).replace(',', '.');
+	}
+	
+	public String getValorFinanciadoComJuros1() {
+		return format1.format(LoanFeature.loan.get(0).getLoanAmountWithInterests()).replace(',', '.');
+	}
+	
+	public String getValorFinanciado2() {
+		return format1.format(LoanFeature.loan.get(1).getAmount()).replace(',', '.');
+	}
+	
+	public String getPcJuros2() {
+		return format2.format(LoanFeature.loan.get(1).getPcOfInterest()).replace(',', '.');
+	}
+	
+	public String getNuParcelas2() {
+		return LoanFeature.loan.get(1).getNumberOfMontlhyInstalments().toString();
+	}
+	
+	public String getVlParcelas2() {
+		return LoanFeature.loan.get(1).getValueOfMontlhyInstalments().toString();
+	}
+	
+	public String getValorFinanciadoComJuros2() {
+		return format1.format(LoanFeature.loan.get(1).getLoanAmountWithInterests()).replace(',', '.');
+	}
+	
+	public String getValorFinanciado3() {
+		return format1.format(LoanFeature.loan.get(2).getAmount()).replace(',', '.');
+	}
+	
+	public String getPcJuros3() {
+		return format2.format(LoanFeature.loan.get(2).getPcOfInterest()).replace(',', '.');
+	}
+	
+	public String getNuParcelas3() {
+		return LoanFeature.loan.get(2).getNumberOfMontlhyInstalments().toString();
+	}
+	
+	public String getVlParcelas3() {
+		return format1.format(LoanFeature.loan.get(2).getValueOfMontlhyInstalments()).replace(',', '.');
+	}
+	
+	public String getValorFinanciadoComJuros3() {
+		return format1.format(LoanFeature.loan.get(2).getLoanAmountWithInterests()).replace(',', '.');
+	}
+	
+	public String getValorFinanciado4() {
+		return format1.format(LoanFeature.loan.get(3).getAmount()).replace(',', '.');
+	}
+	
+	public String getPcJuros4() {
+		return format2.format(LoanFeature.loan.get(3).getPcOfInterest()).replace(',', '.');
+	}
+	
+	public String getNuParcelas4() {
+		return LoanFeature.loan.get(3).getNumberOfMontlhyInstalments().toString();
+	}
+	
+	public String getVlParcelas4() {
+		return format1.format(LoanFeature.loan.get(3).getValueOfMontlhyInstalments()).replace(',', '.');
+	}
+	
+	public String getValorFinanciadoComJuros4() {
+		return format1.format(LoanFeature.loan.get(3).getLoanAmountWithInterests()).replace(',', '.');
+	}
 }
